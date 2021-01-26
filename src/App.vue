8c9fda7b10edcc77a62eb1a8bc9523a98a0b0ea8
Benita Rego <regobenita26@gmail.com>
2021-01-26 13:01:17 +0530

    Commit files

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app class="black">
+    <v-row>
+      <Navbar/>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-row>
+  </v-app>
+</template>
+
+
+
+<script>
+import Navbar from "./components/Navbar"
+
+
+export default {
+  name: 'App',
+
+  components: {
+    // HelloWorld,
+    Navbar
+    
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style lang="css">
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify, {
+    theme: {
+        primary: 'black',
+        info: '#0A5C32',
+        page: '#197747'
+    }
+});
+
+export default new Vuetify({
+});
