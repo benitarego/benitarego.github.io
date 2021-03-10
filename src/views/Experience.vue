5bfe9ad9479d20dba70fde21e0d39d2618178cb0
Benita Rego <42091557+benitarego@users.noreply.github.com>
2021-03-10 20:25:05 +0530

    Update Experience.vue

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -11,7 +11,7 @@
                     <h3 class="mx-3 mt-2 font-weight-medium grey--text">{{ exp.company }}</h3>
                     <h3 class="body-1 pa-3">
                         Developed an application where:
-                        <li>job/internship seekers can find a desired job or an internship with in-built resume created by providing the necessary inputs.</li>
+                        <li>Job/internship seekers can find a desired job or an internship with in-built resume created by providing the necessary inputs.</li>
                         <li>Job/internship recruiters can publicly post a job/internship positions open at their organization and hire seekers applied for the positions posted by them</li>
                         <li>Direct connection between the seekers and recruiters</li>
                         <li>Designed using Dart language along with the Pub package manager for necessary libraries in Flutter SDK and backend using Cloud Firestore and authentication in Firebase</li>
